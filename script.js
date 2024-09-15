5a19186ea7c7d8c17379fe699439c21f7d869072
Bikash Bhagat <bikash.bhagat01@gmail.com>
2024-09-15 21:22:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 function indexOfIgnoreCase(str, subStr) {
-  // Convert both strings to lowercase to ensure case-insensitive comparison
-  const lowerStr = str.toLowerCase();
-  const lowerSubStr = subStr.toLowerCase();
-  
-  // Use the built-in indexOf method to find the first occurrence of subStr in str
-  return lowerStr.indexOf(lowerSubStr);
+	const lowerStr = str.toLowerCase();
+	const lowerSubStr = subStr.toLowerCase();
+
+	return lowerStr.indexOf(lowerSubStr);
 }
 
 // Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+// alert(indexOfIgnoreCase(s1, s2));
