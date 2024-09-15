66d14ea8951bd9afeda0434f724a6710d4fba2ba
Bikash Bhagat <bikash.bhagat01@gmail.com>
2024-09-15 19:59:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+function indexOfIgnoreCase(str, subStr) {
+  // Convert both strings to lowercase to ensure case-insensitive comparison
+  const lowerStr = str.toLowerCase();
+  const lowerSubStr = subStr.toLowerCase();
+  
+  // Use the built-in indexOf method to find the first occurrence of subStr in str
+  return lowerStr.indexOf(lowerSubStr);
 }
 
 // Please do not change the code below
